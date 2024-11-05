9be4e5e827d4a6c4dd98952c8c0beb6543b1563f
Rikhi Singh <114336052+RikhiSingh@users.noreply.github.com>
2024-11-05 17:09:30 -0500

    added comments

--- Easy/1957. Delete Characters to Make Fancy String/sol.py
+++ Easy/1957. Delete Characters to Make Fancy String/sol.py
@@ -2,11 +2,18 @@
 
 class Solution:
     def makeFancyString(self, s: str) -> str:
+        # Initialize the stack
         stack = []
 
+        # Iterate over each character through the string
         for c in s:
+            # We add current character to the stack
             stack.append(c)
 
+            # If stack has more than 3 characters and and last three are same
             if len(stack) >=3 and (stack[-1] == stack[-2] == stack[-3]):
+                # Remove the most recent character (preventing three consecutive same characters)
                 stack.pop()
+                
+        # Join the stack to get the final string
         return "".join(stack)
